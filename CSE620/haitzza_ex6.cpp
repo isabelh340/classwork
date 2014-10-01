8c27673f1c8cbc0d6521c192eb74266cb9630632
haitzza <haitzza@h-0186.lab8.csi.miamioh.edu>
2014-10-01 10:11:34 -0400

    ex6 done

--- CSE620/haitzza_ex6.cpp
+++ CSE620/haitzza_ex6.cpp
@@ -1,29 +1,51 @@
+// copyright Zach Haitz 2014
 #define __CL_ENABLE_EXCEPTIONS
 #include <CL/cl.hpp>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <string>
+
+void printDeviceInfo(cl::Device device) {
+    const std::vector<std::string> deviceHeaders =
+        {"\tDevice name\t: ", "\tFrequency\t: ", "\tCompute Units\t: ",
+         "\tGlobal memory\t: ", "\tLocal memory\t: "};
+    std::cout << deviceHeaders[0] << device.getInfo<CL_DEVICE_NAME>()
+              << std::endl
+              << deviceHeaders[1]
+              << device.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>()
+              << std::endl
+              << deviceHeaders[2]
+              << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>()
+              << std::endl
+              << deviceHeaders[3] << device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>()
+              << std::endl
+              << deviceHeaders[4] << device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>()
+              << std::endl;
+}
+
+void printPlatformInfo(cl::Platform plat) {
+    const std::vector<std::string> platHeaders =
+        {"Vendor\t: ", "Name\t: ", "Version\t: ", "Devices\t: "};
+    std::cout << platHeaders[0]
+              << plat.getInfo<CL_PLATFORM_VENDOR>()
+              << std::endl;
+    std::cout << platHeaders[1]
+              << plat.getInfo<CL_PLATFORM_NAME>()
+              << std::endl;
+    std::cout << platHeaders[2]
+              << plat.getInfo<CL_PLATFORM_VERSION>()
+              << std::endl;
+    std::vector<cl::Device> devices;
+    plat.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+    std::cout << platHeaders[3]
+              << devices.size() << std::endl;
+    std::for_each(devices.begin(), devices.end(), printDeviceInfo);
+}
 
 int main() {
-    const std::vector<std::string> platHeaders =
-        {"Vendor", "Name", "Version", "Devices"};
     std::vector<cl::Platform> platformList;
     cl::Platform::get(&platformList);
-    std::string a;
-    for (uint i = 0; i < platformList.size(); i++) {
-        std::cout << platHeaders[0] << "\t:"
-                  << platformList[i].getInfo<CL_PLATFORM_VENDOR>()
-                  << std::endl;
-        std::cout << platHeaders[1] << "\t:"
-                  << platformList[i].getInfo<CL_PLATFORM_NAME>()
-                  << std::endl;
-        std::cout << platHeaders[2] << "\t:"
-                  << platformList[i].getInfo<CL_PLATFORM_VERSION>()
-                  << std::endl;
-
-        std::vector<cl::Device> deviceList;
-        platformList[i].getDevices(CL_DEVICE_TYPE_ALL, &deviceList);
-        std::cout << platHeaders[3] << "\t:" << deviceList.size() << std::endl;
-    }
-    
+    std::for_each(platformList.begin(), platformList.end(), printPlatformInfo);
     return 0;
 }
